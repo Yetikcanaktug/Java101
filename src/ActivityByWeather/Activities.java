4dcf0334165584ebb2b84d66e3e36d6294e8f23a
Aktug Yetikcan <yetikcan47@gmail.com>
2023-01-08 00:01:06 +0300

    uptade on ActivitesByWeather

--- src/ActivityByWeather/Activities.java
+++ src/ActivityByWeather/Activities.java
@@ -7,9 +7,9 @@ public class Activities {
         int temperature = inp.nextInt();
         if (temperature < 5) {
             System.out.println("You can go to ski. On those weathers, this is best choice for you.");
-        } else if (temperature > 5 && temperature < 15) {
+        } else if (temperature >= 5 && temperature < 15) {
             System.out.println("You can go to the cinema. It's not too cold outside, but on those weathers inside is always better. ");
-        } else if (temperature > 15 && temperature < 25) {
+        } else if (temperature >= 15 && temperature < 25) {
             System.out.println("You can go on a picnic. Enjoy the beautiful weather!");
         } else if (temperature >= 25) {
             System.out.println("You should go swimming! The best solution to cool off in this weather.");
